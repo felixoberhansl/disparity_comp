7a282ed2a568736d702ac59b34e5981e7502544d
ge49puw <manuel.lengl@tum.de>
2019-06-24 18:16:39 +0200

    implemented basic block matching approach

--- disparity_map.m
+++ disparity_map.m
@@ -15,8 +15,8 @@ function [D, R, T] = disparity_map(scene_path)
     
     %% Essentielle Matrix
     % preprocessing images
-    img1gray = rgb2gray(img1);
-    img2gray = rgb2gray(img2);
+    img1gray = double(rgb2gray(img1));
+    img2gray = double(rgb2gray(img2));
     
     % compute harris-features
     features1 = harris_detektor(img1gray);
@@ -38,15 +38,98 @@ function [D, R, T] = disparity_map(scene_path)
     % estimate correct T and R
     [T, R, lambda, M1, M2] = rekonstruktion(T1, T2, R1, R2, correspondences, cam0);
     
-    %% testing the matlab function for disparity map calculation
-    % compute disparity map 
-    disparityRange = [0 48];
-    disparityMap = disparityBM(img1gray,img2gray,'DisparityRange',disparityRange,'UniquenessThreshold',20);
+    % transform T into [m]
+    % -> use cx1 - cx0 to get the distance between the cameras along the
+    % x-axis???????
     
-    % plot
-    figure
-    imshow(disparityMap,disparityRange)
-    title('Disparity Map')
-    colormap jet
+    % ---------------------------------------------------------------------
+    % ---------------------------------------------------------------------
+    
+    %% Disparity Map
+    % rectifying images 
+    % (in order to have two images projected on a virtual
+    % plane, so that there is only movement along the x-axis)
+    
+    % is this step neccessary, or do we only get such images?
+    %----------------------------------------------------------------------
+    
+    
+    % first try on basic block matching approach
+    % -> basically takes a block of pixels and tries to find the best
+    % matching block in the other image (only along the same row, because 
+    % of the rectified images), distance between these matching blocks is
+    % then the disparity
+    
+    disparityMap = zeros(height,width);
+    
+    disparityRange = ceil(doffs);                                    % defines the range where to search
+    
+    blockSize = 11;                                                          % defines the size of the blocks
+    halfBlockSize = (blockSize-1)/2;
+    
+    for m = 1: height
+        minRow = max(1, m-halfBlockSize);
+        maxRow = min(height, m+halfBlockSize);
+        
+        for n = 1: width
+            minCol = max(1, n-halfBlockSize);
+            maxCol = min(width, n+halfBlockSize);
+            
+            minDisp = 0;
+            maxDisp = min(disparityRange, width-maxCol);
+            
+            rightBlock = img2gray(minRow:maxRow, minCol:maxCol);
+            
+            numBlocks = maxDisp - minDisp + 1;
+            
+            blockDiffs = zeros(numBlocks,1);
+            
+            for i = minDisp : maxDisp
+                leftBlock = img1gray(minRow:maxRow, minCol+i : maxCol+i);
+                idxBlock = i - minDisp + 1;
+                blockDiffs(idxBlock) = sum(sum(abs(rightBlock-leftBlock)));
+            end
+            
+            [~, idxSorted] = sort(blockDiffs);
+            
+            matchingBlock = idxSorted(1);
+            
+            disp = matchingBlock + minDisp - 1;
+            
+            % subpixel interpolation
+            if (matchingBlock == 1) || (matchingBlock == numBlocks)
+                disparityMap(m, n) = disp;
+            else
+                
+                C1 = blockDiffs(matchingBlock - 1);
+                C2 = blockDiffs(matchingBlock);
+                C3 = blockDiffs(matchingBlock + 1);
+                
+                % Adjust the disparity by some fraction.
+                % estimating the subpixel location of the true best match.
+                disparityMap(m, n) = disp - (0.5 * (C3 - C1) / (C1 - (2*C2) + C3));
+            end
+        end
+    end
+    
+    
+    figure(1)
+    imshow(disparityMap, []);
+    axis image;
+    colormap('jet')
     colorbar
+            
+            
+            
+%     %% testing the matlab function for disparity map calculation
+%     % compute disparity map 
+%     disparityRange = [0 48];
+%     disparityMap = disparityBM(img1gray,img2gray,'DisparityRange',disparityRange,'UniquenessThreshold',20);
+%     
+%     % plot
+%     figure
+%     imshow(disparityMap,disparityRange)
+%     title('Disparity Map')
+%     colormap jet
+%     colorbar
 end
