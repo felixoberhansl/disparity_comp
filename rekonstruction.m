a8d23affa3f25570b7b10d858bb7a133e856c1ec
Manuel Lengl <manuel.lengl@tum.de>
2019-07-03 15:21:09 +0200

    translated comments

--- rekonstruction.m
+++ rekonstruction.m
@@ -1,47 +1,41 @@
-function [T, R, lambda, M1, M2] = rekonstruktion(T1, T2, R1, R2, Korrespondenzen, K)
-    %% Vorbereitung aus Aufgabe 4.2
-    % T_cell    Cell-Array mit T1 und T2 
-    % R_cell    Cell-Array mit R1 und R2
-    % d_cell    Cell-Array für die Tiefeninformationen
-    % x1        homogene kalibrierte Koordinaten
-    % x2        homogene kalibrierte Koordinaten
+function [T, R, lambda, M1, M2] = rekonstruction(T1, T2, R1, R2, correspondences, K)
     %% Preparation
-    % alle Kombinationsmöglichkeiten für T un R abbilden
+    % all possible combinations for T and R
     T_cell = {T1, T2, T1, T2};
     R_cell = {R1, R1, R2, R2};
     
-    % x1, x2 homogenisieren und kalibrieren
-    x1 = [Korrespondenzen(1:2,:); ones(1, size(Korrespondenzen,2))];
-    x2 = [Korrespondenzen(3:4,:); ones(1, size(Korrespondenzen,2))];
+    % homogenize and calibrate x1, x2
+    x1 = [correspondences(1:2,:); ones(1, size(correspondences,2))];
+    x2 = [correspondences(3:4,:); ones(1, size(correspondences,2))];
     x1 = (K^-1)*x1;              
     x2 = (K^-1)*x2;  
     
-    n = size(Korrespondenzen,2);
+    n = size(correspondences,2);
     d_cell = {zeros(n,2), zeros(n,2), zeros(n,2), zeros(n,2)};
     
-    %% Rekonstruktion
-    N = size(Korrespondenzen,2);
+    %% Rekonstruction
+    N = size(correspondences,2);
     M1 = zeros(N,N+1);
     M2 = zeros(N,N+1);
 
     
-    % 1. M1 und M2 aufstellen
+    % 1. build M1 and M2
     for i = 1:4
         for j = 1:N
             M1(1+(j-1)*3 : 3+(j-1)*3, j) = skew(x2(:,j)) * R_cell{i} * x1(:,j);
-            M1(1+(j-1)*3 : 3+(j-1)*3, N+1) = skew(x2(:,j)) * T_cell{i};       % letzte Spalte in M
+            M1(1+(j-1)*3 : 3+(j-1)*3, N+1) = skew(x2(:,j)) * T_cell{i};       % last col in M
             
             M2(1+(j-1)*3 : 3+(j-1)*3, j) = skew(x1(:,j)) * R_cell{i}.' * x2(:,j);
-            M2(1+(j-1)*3 : 3+(j-1)*3, N+1) = -skew(x1(:,j)) * R_cell{i}.' * T_cell{i};       % letzte Spalte in M
+            M2(1+(j-1)*3 : 3+(j-1)*3, N+1) = -skew(x1(:,j)) * R_cell{i}.' * T_cell{i};       % last col in M
         end
         
-        % 2. d1 und d2 ermitteln
+        % 2. compute d1 and d2
         [~, ~, V1] = svd(M1);
         [~, ~, V2] = svd(M2);
         
         d1 = V1(:, N+1);
         d2 = V2(:, N+1);
-        % normieren
+        % norm
         d1 = d1/(d1(N+1));
         d2 = d2/(d2(N+1));
         
@@ -51,7 +45,7 @@ function [T, R, lambda, M1, M2] = rekonstruktion(T1, T2, R1, R2, Korrespondenzen
     end
 
     
-    % lambdas mit meisten pos. Tiefeninformationen finden -> sign -> sum -> max 
+    % find lambdas with the most pos. values -> sign -> sum -> max 
     for i = 1:4
         depth(i) = sum(sum(sign(d_cell{i})));
     end
